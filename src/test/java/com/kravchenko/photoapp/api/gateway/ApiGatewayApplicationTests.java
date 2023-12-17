52fe1072513573d0274edab4923a93b1e465d113
Yehor Kravchenko <brazuca.egor@gmail.com>
2023-12-17 17:58:58 +0200

    Initial commit

---
+++ src/test/java/com/kravchenko/photoapp/api/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kravchenko.photoapp.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
